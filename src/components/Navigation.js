1c711f5b3e7c465a345ae138ffc6175703b3b5aa
Laetitia Bouslah <laetitia.bouslah@free.fr>
2023-08-06 16:27:31 +0200

    Copie des fichiers images dans nouveau dossier ecf

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom"
 
 export const Navigation = () => {
     return (
+        
         <header className=' md:flex justify-between items-center mb-5 border 1px bg-[#d9d9d9] rounded'>
             <img className='h-48 w-50 md:h-full md:w-48 ml-20 mt-3 mb-3' src='images/Logo.png' alt='Logo du site'/>
             <nav className='flex justify-center items-center'>
